9714934015ef25fa62290376bf8c951885f8ab04
Oracy <oramartos_21@hotmail.com>
2018-12-16 03:51:46 -0200

    Finish

--- BackupScript.sh
+++ BackupScript.sh
@@ -1,6 +1,20 @@
+#!/bin/sh
 # Linux commands below to schedule to run script every two days 22:30
 # crontab -e
 # m h  dom mon dow   command
 # 30 22 */2 * * ./DatabaseBackup.sh
+# DatabaseBackup.sh
 
-sudo mysqldump -u root --all-databases | gzip > "database_data_backup-"`date +"%d-%m-%Y"`".sql.gz"
+DATA=`date +"%d-%m-%Y"`
+
+# NOME armazena o nome do arquivo de backup, neste caso
+# o diretório é /home/oracy/Backup, configure aqui o seu
+# diretório de backup
+NOME="/home/oracy/Backup/database_data_backup-$DATA.sql.gz"
+
+# variáveis do MySQL
+HOST="localhost"
+USER="root"
+PASSWORD="211192"
+
+mysqldump -h $HOST -u $USER -p$PASSWORD --all-databases | gzip > $NOME 
